f4f3297475d33ec81978a26ac2a34e5fc97d0c2a
Youssef Asaad <youssef.a.asaad@gmail.com>
2024-10-29 00:31:13 -0400

    basic monkey test case passing

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"fmt"
 	"monkey-interpreter/token"
 	"testing"
 )
@@ -63,7 +64,7 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
-
+    fmt.Println(tok)
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got %q", i, tt.expectedType, tok.Type)
 		}
