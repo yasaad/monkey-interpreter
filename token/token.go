227776871f94898eb9fc9fa07064d67a5bbe03de
Youssef Asaad <youssef.a.asaad@gmail.com>
2024-10-29 13:42:03 -0400

    added symbols

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 123545
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
@@ -34,13 +41,13 @@ const (
 )
 
 var keywords = map[string]TokenType{
-  "fn": FUNCTION,
-  "let": LET,
+	"fn":  FUNCTION,
+	"let": LET,
 }
 
 func LookupIdent(ident string) TokenType {
-  if tok, ok := keywords[ident]; ok {
-    return tok
-  }
-  return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
