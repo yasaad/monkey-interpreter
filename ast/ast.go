2df363fed29682207b3e6cb8744aad742e7da260
Youssef Asaad <youssef.a.asaad@gmail.com>
2024-11-14 17:12:50 -0800

    let statement parser with erroring tests

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "monkey-interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (ls *Identifier) expressionNode()      {}
+func (ls *Identifier) TokenLiteral() string { return ls.Token.Literal }
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
